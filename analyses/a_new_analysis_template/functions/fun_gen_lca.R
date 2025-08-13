d30e2bd054215e19477c66c634ccb9e1ca8b9795
JeremyDanielCooper <41076510+JeremyDanielCooper@users.noreply.github.com>
2025-08-12 20:12:49 -0700

    updated BFA vulnerabilities to collapse categories for latrine location

--- analyses/a_new_analysis_template/functions/fun_gen_lca.R
+++ analyses/a_new_analysis_template/functions/fun_gen_lca.R
@@ -1,20 +1,7 @@
 
 
-
-################################################################################
-# GENERATE LCA OUTPUT
-################################################################################
-
-###################################
-# RUN SETUP
-
-
-###################################
-# DEFINE FUNCTION TO GENERATE LCA OUTPUT
-###################################
-
-
 fun_gen_lca <- function(df=NULL, input_vars=NULL, nreps=NULL, output_path=NULL){
+
 
   #
   stratum = df$strata[[1]]
@@ -33,9 +20,39 @@ fun_gen_lca <- function(df=NULL, input_vars=NULL, nreps=NULL, output_path=NULL){
   #
   f <- do.call(cbind, lca_input[,lca_input_names])~1
 
-  ### FITTING N CLASSES
-  for (i in 2:10){
-    print(paste0(stratum, "_", i, "_classes"))
+
+  pacman::p_load(foreach, doParallel, doRNG, progress)
+
+  nc <- max(1, parallel::detectCores() - 1)
+  cl <- parallel::makeCluster(nc)
+  on.exit(try(parallel::stopCluster(cl), silent = TRUE), add = TRUE)
+  doParallel::registerDoParallel(cl)
+
+  classes <- 2:10
+
+
+  pb <- progress_bar$new(
+    format = "  Fitting models [:bar] :percent eta: :eta",
+    total = length(classes),
+    clear = FALSE,
+    width = 60
+  )
+
+
+  combiner <- function(acc, elem) {
+    pb$tick()
+    c(acc, list(elem))
+  }
+
+
+  # %dorng% ensures reproducible RNG streams in parallel
+  # set.seed(99)  # your master seed
+  res <- foreach::foreach(
+    i = classes,
+    .packages = c("poLCA"),
+    .combine  = combiner,
+    .init     = list()
+  ) %dorng% {
 
     name <- paste0(stratum, "_LCA", i)
     data <- poLCA(f, data=lca_input, nclass=i, maxiter=3000, nrep=nreps)
@@ -47,5 +64,20 @@ fun_gen_lca <- function(df=NULL, input_vars=NULL, nreps=NULL, output_path=NULL){
 
   }
 
-}
 
+  # ### FITTING N CLASSES
+  # for (i in 2:10){
+  #
+  #   print(paste0(stratum, "_", i, "_classes"))
+  #
+  #   name <- paste0(stratum, "_LCA", i)
+  #   data <- poLCA(f, data=lca_input, nclass=i, maxiter=3000, nrep=nreps)
+  #   assign(name, data)
+  #
+  #   path = paste0(output_path, "nreps", nreps, "/", name, ".rds")
+  #   dir.create(dirname(path), showWarnings = F, recursive = T)
+  #   saveRDS(data, path)
+  #
+  # }
+
+}
